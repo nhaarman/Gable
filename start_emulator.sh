a0e8b73492e9f5a33e414cc90a25297cd56fbf19
Niek Haarman <haarman.niek@gmail.com>
2015-02-21 14:42:36 +0100

    Only start emulator if PR

---
+++ start_emulator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set +e
+
+echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
